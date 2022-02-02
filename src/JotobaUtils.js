894fdc26e8694b794cddb1dccac820e7ae840036
clnhs <git@colinhs.com>
2022-02-01 21:06:40 -0500

    code: removed old parsePos function

--- src/JotobaUtils.js
+++ src/JotobaUtils.js
@@ -31,33 +31,3 @@ export const parsePos = (pos, short = false, kana = false, lang = "en") => {
 
     return posStr;
 };
-
-export const parsePos_old = part => {
-    const partsOfSpeech = [];
-
-    if (typeof part === "string") {
-        if (part !== "AuxiliaryVerb") {
-            partsOfSpeech.push(`Auxiliary verb`);
-        } else partsOfSpeech.push(part);
-    } else if (Object.hasOwn(part, "Verb")) {
-        if (typeof part.Verb === "string")
-            partsOfSpeech.push(part.Verb + " verb");
-        else if (Object.hasOwn(part.Verb, "Godan")) {
-            partsOfSpeech.push(`Godan verb`);
-        } else if (Object.hasOwn(part.Verb, "Irregular")) {
-            if (part.Verb.Irregular === "NounOrAuxSuru")
-                partsOfSpeech.push(`する verb`);
-        }
-    } else if (Object.hasOwn(part, "Noun")) {
-        if (part.Noun === "Normal") partsOfSpeech.push(`Noun`);
-        else partsOfSpeech.push(part.Noun.toLowerCase() + " noun");
-    } else if (Object.hasOwn(part, "Adjective"))
-        if (part.Adjective === "PreNounVerb")
-            partsOfSpeech.push(`Pre-noun/verb adj.`);
-        else if (part.Adjective === "Keiyoushi")
-            partsOfSpeech.push(`I-adj. 【Keiyoushi】`);
-        else partsOfSpeech.push(`${part.Adjective} adj.`);
-    else partsOfSpeech.push(JSON.stringify(part.Verb));
-
-    return partsOfSpeech;
-};
