06519d21dec81028a2055f2f830b29a1d880d937
clnhs <git@colinhs.com>
2022-02-02 10:58:27 -0500

    fix: parts of speech parsing issue created by last "fix" of parts of speech parsing issue. Testing when?

--- src/JotobaUtils.js
+++ src/JotobaUtils.js
@@ -6,7 +6,10 @@ export const parseReadings = readings => {
         .join(", ");
 };
 
-import {simple as simplePosData, detailed as detailedPosData} from "./PartsOfSpeech.json";
+import {
+    simple as simplePosData,
+    detailed as detailedPosData,
+} from "./PartsOfSpeech.json";
 
 export const parsePos = (pos, short = false, kana = false, lang = "en") => {
     let posStr = "missing";
@@ -14,13 +17,16 @@ export const parsePos = (pos, short = false, kana = false, lang = "en") => {
     if (typeof pos === "object") {
         const mainPart = Object.keys(pos)[0];
         const subPart = Object.values(pos)[0];
-        if (subPart){
+        if (subPart) {
             if (typeof subPart === "string")
                 posMatch = detailedPosData[mainPart][subPart];
-            else
-                posMatch = detailedPosData[mainPart][Object.keys(subPart)[0]].types[Object.values(subPart)[0]]
-        }
-        else posMatch = detailedPosData[mainPart];
+            else {
+                const category = detailedPosData[mainPart];
+                const subCategory = category[Object.keys(subPart)[0]];
+                if (subCategory)
+                    posMatch = subCategory.types[Object.values(subPart)[0]];
+            }
+        } else posMatch = detailedPosData[mainPart];
     } //string
     else posMatch = simplePosData[pos];
 
