07a6d6d67c90483a69d63cd15288737dba1ea2b7
clnhs <git@colinhs.com>
2022-02-24 20:04:54 -0500

    refactor: removed old code.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { List, showToast, Toast, getPreferenceValues } from "@raycast/api";
 
 import { useState, useEffect, useRef } from "react";
-import fetch, { AbortError } from "node-fetch";
+import { AbortError } from "node-fetch";
 import { nanoid } from "nanoid";
 
 import WordListItem from "./components/ListItems/WordListItem";
@@ -108,47 +108,5 @@ function useSearch() {
     return {
         state: state,
         search: search,
-    };
-}
-
-async function performSearch(
-    searchText: string,
-    signal: AbortSignal
-): Promise<SearchResult> {
-    const { userLanguage, useEnglishFallback } =
-        getPreferenceValues<Preferences>();
-
-    const response = await fetch("https://jotoba.de/api/search/words", {
-        method: "POST",
-        signal: signal,
-        headers: {
-            "Content-Type": "application/json",
-        },
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify({
-            query: searchText,
-            no_english: !useEnglishFallback,
-            language: userLanguage,
-        }),
-    });
-
-    if (searchText && !response.ok) {
-        return Promise.reject(response.statusText);
-    }
-
-    const responseJSON = (await response.json()) as Json;
-
-    const words = (responseJSON?.words as JotobaWord[]) ?? [];
-    const kanji = (responseJSON?.kanji as JotobaKanji[]) ?? [];
-
-    return {
-        words: words.map(wordEntry => ({
-            id: nanoid(),
-            ...wordEntry,
-        })),
-        kanji: kanji.map(kanjiEntry => ({
-            id: nanoid(),
-            ...kanjiEntry,
-        })),
     };
 }
