d4c3e4a823601a54f98ff1c078bec7063ecbfef3
clnhs <git@colinhs.com>
2022-09-01 16:23:42 -0400

    parseReadings can now parse into an array; parseReadings now uses "〜" instead of "ー" for word endings; reformat

--- src/JotobaUtils.ts
+++ src/JotobaUtils.ts
@@ -1,21 +1,30 @@
 import * as fs from "fs";
 import { environment } from "@raycast/api";
 
-export const parseReadings = (readings: string[]) => {
+export const parseReadings = (readings: string[], asArray?: false|boolean) => {
     if (!readings || readings.length === 0) return "";
 
+    if (asArray)
+        return readings.map(reading =>
+            reading.replace(".", "・").replace("-", "〜")
+        );
+
     return readings
-        .map(reading => reading.replace(".", "・").replace("-", "ー"))
+        .map(reading => reading.replace(".", "・").replace("-", "〜"))
         .join(", ");
 };
 
-export const parsePos = (unparsedPosData: string[] | PartOfSpeech, type?: string): string => {
-    const posDataBuffer = fs.readFileSync(`${environment.assetsPath}/PartsOfSpeech.json`);
+export const parsePos = (
+    unparsedPosData: string[] | PartOfSpeech,
+    type?: string
+): string => {
+    const posDataBuffer = fs.readFileSync(
+        `${environment.assetsPath}/PartsOfSpeech.json`
+    );
 
-    const {
-        simple: simplePosData,
-        detailed: detailedPosData,
-    } = JSON.parse(posDataBuffer.toString());
+    const { simple: simplePosData, detailed: detailedPosData } = JSON.parse(
+        posDataBuffer.toString()
+    );
 
     let posStr = "missing";
     let posMatch = null;
@@ -30,20 +39,23 @@ export const parsePos = (unparsedPosData: string[] | PartOfSpeech, type?: string
                     posMatch = detailedPosData[pos][posDetailData];
                 } else {
                     const potentialPosGroupMatches = detailedPosData[pos];
-                    Object.entries(posDetailData).map((detailData) => {
+                    Object.entries(posDetailData).map(detailData => {
                         const posGroupName: Json = detailData[0];
                         const posGroupType: Json = detailData[1];
 
-                        if (potentialPosGroupMatches[posGroupName] && posGroupType)
-                            posMatch = potentialPosGroupMatches[posGroupName].types[posGroupType];
-
+                        if (
+                            potentialPosGroupMatches[posGroupName] &&
+                            posGroupType
+                        )
+                            posMatch =
+                                potentialPosGroupMatches[posGroupName].types[
+                                    posGroupType
+                                ];
                     });
                 }
-            } else
-                posMatch = detailedPosData[pos];
+            } else posMatch = detailedPosData[pos];
         });
-    } else
-        posMatch = simplePosData[unparsedPosData];
+    } else posMatch = simplePosData[unparsedPosData];
 
     if (posMatch) {
         switch (type) {
@@ -57,7 +69,11 @@ export const parsePos = (unparsedPosData: string[] | PartOfSpeech, type?: string
                 posStr = posMatch.kana?.long || posMatch.long;
                 break;
             case "kshort":
-                posStr = posMatch.kana?.short || posMatch.short || posMatch.kana?.long || posMatch.long;
+                posStr =
+                    posMatch.kana?.short ||
+                    posMatch.short ||
+                    posMatch.kana?.long ||
+                    posMatch.long;
                 break;
             default:
                 posStr = posMatch.long || posMatch;
